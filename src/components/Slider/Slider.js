9032171001678860a2fb8cfc2fd15c0938bc9031
lukasZelinka <77839958+lukasZelinka@users.noreply.github.com>
2023-02-09 20:39:40 +0100

    ul commented

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,34 +1,34 @@
 import React from "react";
 import "./Slider.css";
 
-class Slider extends React.Component {
-  videoUrl(xSource) {
-    document.getElementById("slider").src = xSource;
-  }
+function Slider() {
+  const slider = React.useRef();
 
-  render() {
-    return (
-      <section className="slider">
-        <video muted autoPlay loop id="slider">
-          <source src="video1.mp4" type="video/mp4" />
-        </video>
-        <ul className="navigation">
-          <li onClick={() => this.videoUrl("video1.mp4")}>
-            <img src="thumb1.png" alt="sth" />
-          </li>
-          <li onClick={() => this.videoUrl("video2.mp4")}>
-            <img src="thumb1.png" alt="sth" />
-          </li>
-          <li onClick={() => this.videoUrl("video3.mp4")}>
-            <img src="thumb1.png" alt="sth" />
-          </li>
-          <li onClick={() => this.videoUrl("video4.mp4")}>
-            <img src="thumb1.png" alt="sth" />
-          </li>
-        </ul>
-      </section>
-    );
-  }
+  // function videoUrl(xSource) {
+  //   slider.current.src = xSource;
+  // }
+
+  return (
+    <section className="slider">
+      <video muted autoPlay loop id="slider" ref={slider}>
+        <source src="video1.mp4" type="video/mp4" />
+      </video>
+      {/* <ul className="navigation">
+        <li onClick={() => videoUrl("video1.mp4")}>
+          <img src="thumb1.png" alt="sth" />
+        </li>
+        <li onClick={() => videoUrl("video2.mp4")}>
+          <img src="thumb1.png" alt="sth" />
+        </li>
+        <li onClick={() => videoUrl("video3.mp4")}>
+          <img src="thumb1.png" alt="sth" />
+        </li>
+        <li onClick={() => videoUrl("video4.mp4")}>
+          <img src="thumb1.png" alt="sth" />
+        </li>
+      </ul> */}
+    </section>
+  );
 }
 
 export { Slider };
